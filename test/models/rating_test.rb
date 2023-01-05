82eb1d4fe728e1ba225c1391609f182f6e226050
Segundo Rebaza <alvaro.rp1593@gmail.com>
2023-01-04 23:41:21 -0500

    write code to pass tests

--- test/models/rating_test.rb
+++ test/models/rating_test.rb
@@ -7,7 +7,7 @@ class RatingTest < ActiveSupport::TestCase
 
   test 'valid if score is between 0 and 10' do
     0.upto(10).each do |i|
-      rating = Rating.new(score: 10)
+      rating = Rating.new(score: i)
       rating.valid?
       assert_empty rating.errors[:score]
     end
